e9cc321552ed989766a1de469f9e318e9ebef7d1
Frank <frank.feng@york.ac.uk>
2018-01-18 10:46:26 +0000

    add draft flat oasis attributes, some of them need to be nested

--- app/forms/hyrax/material_form.rb
+++ app/forms/hyrax/material_form.rb
@@ -3,22 +3,37 @@
 module Hyrax
   class MaterialForm < Hyrax::Forms::WorkForm
     self.model_class = ::Material
-    self.terms = [:material_type,
-                   :material_title,
-                   :material_creator,
-                   :material_funder,
-                   :material_format,
-                   :material_datatype,
-                   :material_linguistictarget,
-                   :material_sourcelanguage,
-                   :material_notes,
-                   :material_rights,
-                   :participants_type,
-                   :participants_firstlanguage,
-                   :participants_targetlanguage,
-                   :participants_age
-    ]
+    self.terms = [:material_creator,
+                  :material_type,
+                  :material_general_research_area,
+                  :material_type_of_file,
+                  :material_datatype,
+                  :material_linguistictarget,
+                  :material_funder,
+                  :material_title,
+                  :material_notes,
+                  :participants_type,
+                  :participants_firstlanguage,
+                  :participants_targetlanguage,
+                  :participants_age,
+                  :participants_gender,
+                  :participants_proficiency,
+                  :participants_type_of_instruction,
+                  :participants_amount_of_instruction,
+                  :participants_length_of_residence,
+                  :participants_age_of_learners_of_arrival,
 
-    self.required_fields = [:material_type,:material_title]
+                  :participants_amount_of_prior_instruction,
+
+                  :participants_time_spent_using_l2,
+                  :participants_domain_of_use,
+
+                  :licence,
+                  :emails,
+                  :licence_agreement,
+                  :comments
+        ]
+
+    self.required_fields = [:material_creator,:material_type,:material_general_research_area,:material_type_of_file]
   end
 end
--- app/models/material.rb
+++ app/models/material.rb
@@ -14,32 +14,52 @@ class Material < ActiveFedora::Base
   # schema (by adding accepts_nested_attributes)
   include ::Hyrax::BasicMetadata
 
+  #--------------------------
   #material type group
-  property :material_type, predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
-  property :material_title, predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
-  property :material_creator, predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
-  property :material_funder, predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
-  property :material_format, predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
-  property :material_datatype, predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
-  property :material_linguistictarget, predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
-  property :material_sourcelanguage, predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
-  property :material_notes, predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
-  property :material_rights, predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
-  property :participants_type, predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
-  property :participants_firstlanguage, predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
-  property :participants_targetlanguage, predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
-  property :participants_age, predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
+  property :material_creator,                  predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
+  property :material_type,                      predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
+  property :material_general_research_area,   predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
+  property :material_type_of_file,             predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
 
+  #--------------------------
   #further details group
-  
+  property :material_datatype,                  predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
+  property :material_linguistictarget,         predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
+  property :material_funder,                     predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
+  property :material_title,                      predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
+  property :material_notes,                      predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
+
+  #--------------------------
   #participants group
+  property :participants_type,                           predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
+  property :participants_firstlanguage,                 predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
+  property :participants_targetlanguage,                predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
+  property :participants_age,                            predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: false
+  property :participants_gender,                         predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: false
+  property :participants_proficiency,                    predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
+  property :participants_type_of_instruction,           predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
+  property :participants_amount_of_instruction,         predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: false
+  property :participants_length_of_residence,           predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: false
+  property :participants_age_of_learners_of_arrival,   predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: false
 
-  #publication group
+  # need to add length of Prior Instruction and Type of Prior Instruction seperately
+  property :participants_amount_of_prior_instruction,  predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
 
-  #acknowledgements group
+  property :participants_time_spent_using_l2,           predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: false
+  property :participants_domain_of_use,                  predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
 
+  #--------------------------
+  #publication and acknowledgements group
+  property :relateditem_type,                  predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
+
+  #--------------------------
   #communication / licence group
+  property :licence,                  predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
+  property :emails,                   predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
 
+  #--------------------------
   #Submit group
+  property :licence_agreement,       predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
+  property :comments,                 predicate: ::RDF::URI.new('http://dlib.york.ac.uk/ontologies#oasis'), multiple: true
 
 end
