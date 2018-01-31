87fe091ff8ec90959c26831ea322a337f372a7e0
Frank <frank.feng@york.ac.uk>
2018-01-31 11:39:29 +0000

    make homepage title text clickable

--- app/assets/javascripts/oasis.js
+++ app/assets/javascripts/oasis.js
@@ -1,3 +1,16 @@
 $(function() {
     $( "#accordion" ).accordion();
+
+    $(".homepage_div").click(function() {
+        var status_elt = $(this).find('input');
+        if (status_elt.val() == "open") {
+            $(this).css('height', '2.5em');
+            status_elt.val('close');
+        } else {
+            $(this).css('height', 'auto');
+            status_elt.val('open');
+        }
+        return false;
+    });
 });
+
