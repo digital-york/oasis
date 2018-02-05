698fb3a812b211c8702d168caf526a65a94a505f
Frank <frank.feng@york.ac.uk>
2018-02-05 13:38:25 +0000

    update OASIS fields

--- app/forms/hyrax/summary_form.rb
+++ app/forms/hyrax/summary_form.rb
@@ -6,53 +6,57 @@ module Hyrax
     self.terms = [:summary_writer,
                   :title_of_summary,
                   :publication_identifier,
-
-                  :summary_general_research_area,
-                  :summary_linguistictarget,
-                  :participants_targetlanguage,
-                  :participants_firstlanguage,
-
-                  :publication_date,
-                  :publication_title,
-
-                  :participants_type,
-                  :participants_gender,
-                  :participants_proficiency,
-                  :participants_domain_of_use,
-
-                  :conference_name,
-                  :material_on_IRIS,
-
-
-                  :summary_notes,
-
-                  :participants_age,
-                  :participants_type_of_instruction,
-                  :participants_amount_of_instruction,
-                  :participants_length_of_residence,
-                  :participants_age_of_learners_of_arrival,
-
-                  :participants_amount_of_prior_instruction,
-
-                  :participants_time_spent_using_l2,
-
                   :publication_author,
+                  :publication_title,
+                  :publication_date,
                   :publication_journal_name,
                   :publication_volume,
                   :publication_issue,
                   :publication_pages_from,
                   :publication_pages_to,
-
-
+                  :summary_general_research_area,
+                  :summary_linguistictarget,
+                  :participants_type,
+                  :participants_firstlanguage,
+                  :participants_targetlanguage,
+                  :participants_age,
+                  :participants_gender,
+                  :participants_proficiency,
+                  :participants_type_of_instruction,
+                  :participants_amount_of_instruction,
+                  :participants_length_of_residence,
+                  :participants_age_of_learners_of_arrival,
+                  :participants_amount_of_prior_instruction,
+                  :participants_time_spent_using_l2,
+                  :participants_domain_of_use,
+                  :participants_year_of_teaching_experience,
+                  :material_on_IRIS,
                   :licence,
-                  :emails,
                   :licence_agreement,
-                  :comments
+                  :emails,
+                  :notification_of_download,
+                  :summary_notes,
+                  :conference_name
     ]
 
     self.required_fields = [:summary_writer,
                             :title_of_summary,
+                            :publication_identifier,
                             :publication_author,
-                            :summary_general_research_area]
+                            :publication_title,
+                            :publication_date,
+                            :publication_journal_name,
+                            :publication_volume,
+                            :publication_issue,
+                            :publication_pages_from,
+                            :publication_pages_to,
+                            :summary_general_research_area,
+                            :participants_type,
+                            :participants_firstlanguage,
+                            :participants_targetlanguage,
+                            :material_on_IRIS,
+                            :emails,
+                            :notification_of_download
+                         ]
   end
 end
