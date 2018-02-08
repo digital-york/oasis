590eb2ea7c7d80fec1c67ca8ea1d23b8679badb0
Frank <frank.feng@york.ac.uk>
2018-02-08 11:46:13 +0000

    add generic local authority service

---
+++ app/services/generic_local_authority_service.rb
@@ -0,0 +1,16 @@
+module GenericLocalAuthorityService
+  mattr_accessor :authority
+
+  def self.select_all_options(key)
+    self.authority = Qa::Authorities::Local.subauthority_for(key)
+
+    authority.all.map do |element|
+      [element[:label], element[:id]]
+    end
+  end
+
+  def self.label(id)
+    authority.find(id).fetch('term')
+  end
+  
+end
