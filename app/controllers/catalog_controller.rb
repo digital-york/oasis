26ec7008bec07b80ecf965ab410dc96f0bc51e30
Sebastian Pałucha <sebastian.palucha@durham.ac.uk>
2018-02-25 14:52:57 +0000

    Reorganise and add missing facets
    Related to #33
    - configure missing facets
    - reorganise facets
    - correct facets labels
    - make missing fields sortable, facetable
    
    Know issues:
    Authorities are displayed as index numbers not string values.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -43,23 +43,26 @@ class CatalogController < ApplicationController
     config.index.display_type_field = solr_name("has_model", :symbol)
     config.index.thumbnail_field = 'thumbnail_path_ss'
 
-    #Oasis fields
+    # Oasis fields
     config.index.summary_general_research_area_field = solr_name('summary_general_research_area_value', :stored_searchable)
     config.index.participants_targetlanguage_field   = solr_name('participants_targetlanguage_value', :stored_searchable)
 
     # solr fields that will be treated as facets by the blacklight application
     # The ordering of the field names is the order of the display
-    config.add_facet_field solr_name('publication_author',              :facetable), label: "Author", limit: 5
     config.add_facet_field solr_name('summary_general_research_area', :facetable), label: "Topic", limit: 5
-    config.add_facet_field solr_name('summary_linguistictarget',       :facetable), label: "Language feature", limit: 5
-    config.add_facet_field solr_name('participants_targetlanguage',    :facetable), label: "Language Being Learned", limit: 5
-    config.add_facet_field solr_name('participants_firstlanguage',     :facetable), label: "First Language(s) of Learners", limit: 5
-    config.add_facet_field solr_name('publication_date',   :facetable), label: "Date of Publication", limit: 5
-    config.add_facet_field solr_name('publication_journal_name',   :facetable), label: "Journal of Publication", limit: 5
-    config.add_facet_field solr_name('participants_gender', :facetable), label: "Gender of Learners", limit: 5
+    config.add_facet_field solr_name('participants_type', :facetable), label: "Participant type", limit: 5
+    config.add_facet_field solr_name('participants_targetlanguage', :facetable), label: "Language Being Learned", limit: 5
+    config.add_facet_field solr_name('summary_linguistictarget', :facetable), label: "Language feature", limit: 5
+    config.add_facet_field solr_name('participants_age', :facetable), label: "Age of learner", limit: 5
     config.add_facet_field solr_name('participants_proficiency', :facetable), label: "Proficiency of Learners", limit: 5
+    config.add_facet_field solr_name('participants_firstlanguage', :facetable), label: "First Language of Learners", limit: 5
+    config.add_facet_field solr_name('participants_gender', :facetable), label: "Gender of Learners", limit: 5
     config.add_facet_field solr_name('participants_domain_of_use', :facetable), label: "Context of Language Use", limit: 5
     config.add_facet_field solr_name('of_likely_interest_to', :facetable), label: "Of likely interest to", limit: 5
+    config.add_facet_field solr_name('materials_on_iris', :facetable), label: "Materials on IRIS", limit: 5
+    config.add_facet_field solr_name('publication_date', :facetable), label: "Date of Publication", limit: 5
+    config.add_facet_field solr_name('publication_author', :facetable), label: "Author of publication", limit: 5
+    config.add_facet_field solr_name('publication_journal_name', :facetable), label: "Journal of Publication", limit: 5
 
     # The generic_type isn't displayed on the facet list
     # It's used to give a label to the filter that comes from the user profile
