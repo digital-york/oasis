3eb6f8cc0ec69ed7d6b7e9426626d60b2ce917bc
Frank <frank.feng@york.ac.uk>
2018-03-12 14:51:01 +0000

    add customized indexer to index authority labels

--- app/indexers/summary_indexer.rb
+++ app/indexers/summary_indexer.rb
@@ -9,11 +9,28 @@ class SummaryIndexer < Hyrax::WorkIndexer
   # this behavior
   include Hyrax::IndexesLinkedMetadata
 
-
-  # Uncomment this block if you want to add custom indexing behavior:
-  # def generate_solr_document
-  #  super.tap do |solr_doc|
-  #    solr_doc['my_custom_field_ssim'] = object.my_custom_property
-  #  end
-  # end
+  # add custom indexing behavior for authority fields:
+  def generate_solr_document
+   super.tap do |solr_doc|
+     # add authority labels into solr
+     OasisAuthorityMapping.authority_mapping_solr2filename.keys.each do |field_name|
+       authority_filename = OasisAuthorityMapping.authority_mapping_solr2filename[field_name]
+       begin
+         unless object[field_name].nil?
+           if object[field_name].is_a? String
+             solr_doc[field_name+'_label_tesim'] = GenericLocalAuthorityService.id_to_label(authority_filename, object[field_name].to_i)
+           else
+             labels = []
+             object[field_name].each do |id|
+               labels << GenericLocalAuthorityService.id_to_label(authority_filename, id.to_i)
+             end
+             solr_doc[field_name+'_label_tesim'] = labels
+           end
+         end
+       rescue
+         Rails.logger.error('Failed while indexing label for ' + field_name)
+       end
+     end
+   end
+  end
 end
