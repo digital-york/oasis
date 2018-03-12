52ca6e7059c11fdeed76c148c0f34acf9e0d3efe
Frank <frank.feng@york.ac.uk>
2018-03-12 14:59:14 +0000

    add oasis text fields to Solr search parameter

--- app/search_builders/oasis/oasis_search_builder.rb
+++ app/search_builders/oasis/oasis_search_builder.rb
@@ -1,11 +1,20 @@
 module Oasis
   class OasisSearchBuilder < Hyrax::CatalogSearchBuilder
-    self.default_processor_chain += [:add_authority_label_field_to_query]
+    self.default_processor_chain += [:add_authority_label_field_to_query, :add_oasis_text_fields_to_query]
 
+    # Add OASIS authority label fields to Solr query parameters
     def add_authority_label_field_to_query(solr_parameters)
       OasisAuthorityMapping.authority_mapping_solr2filename.keys.each do |field_name|
         solr_parameters[:qf] += ' %s_label_tesim' % field_name
       end
+    end
+
+    # Add OASIS text fields to Solr query parameters
+    def add_oasis_text_fields_to_query(solr_parameters)
+      solr_parameters[:qf] += ' publication_author_tesim'
+      solr_parameters[:qf] += ' publication_title_tesim'
+      solr_parameters[:qf] += ' summary_writer_tesim'
+      solr_parameters[:qf] += ' title_of_summary_tesim'
     end
   end
 end
