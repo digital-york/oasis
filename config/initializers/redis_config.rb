e96c5f26bf67d3d369109bddd68bff7eff0405bc
Frank <frank.feng@york.ac.uk>
2018-05-23 09:07:38 +0000

    Use redis defined in redis.yml for Resque

--- config/initializers/redis_config.rb
+++ config/initializers/redis_config.rb
@@ -1,3 +1,4 @@
 require 'redis'
 config = YAML.safe_load(ERB.new(IO.read(Rails.root.join('config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
 Redis.current = Redis.new(config.merge(thread_safe: true))
+Resque.redis = Redis.current
