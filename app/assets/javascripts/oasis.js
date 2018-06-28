cf80f77f424733560255c511f9847ccd3fca5e87
Frank <frank.feng@york.ac.uk>
2018-06-28 08:46:08 +0000

    stored ordered summary writer in summary_summary_writer_all as JSON and restore the order on load

--- app/assets/javascripts/oasis.js
+++ app/assets/javascripts/oasis.js
@@ -20,7 +20,7 @@ $(function() {
     $("#homepage_search").click(function() {
         window.location.href = '/catalog';
     });
-    
+
     <!-- Show hiden IRIS url field -->
     if( $("#summary_materials_on_iris_yes").val()=='Yes'){
         $("#iri_url_div").css("display","block");
@@ -34,5 +34,28 @@ $(function() {
         if( $(this).val()=='No'){
             $("#iri_url_div").css("display","none");
         }
+    });
+
+    <!-- applying order from summary_summary_writer_all -->
+    var all_writers = JSON.parse($('#summary_summary_writer_all').val());
+    if(all_writers) {
+        for(var i in all_writers) {
+            // update all writers in the order defined in summary_summary_writer_all
+            if(all_writers[i]!='') {
+                $('.summary_summary_writer:eq(' + (parseInt(i) + 1) + ')').val(all_writers[i]);
+            }
+        }
+    }
+
+    // before submitting changes, update summary_summary_writer_all first.
+    $("#with_files_submit").click(function(){
+        var authors = [];
+        $("input[name='summary[summary_writer][]']").each(function() {
+            if($(this).val()!='') {
+                authors.push($(this).val());
+            }
+        });
+        // update hidden field 'summary_summary_writer_all' value
+        $('#summary_summary_writer_all').val(JSON.stringify(authors));
     });
 });
