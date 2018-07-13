737d362d6c17b46f6f298aaead066d60cb437b9e
Frank <frank.feng@york.ac.uk>
2018-07-13 13:05:18 +0000

    use OASIS email

--- config/initializers/mailboxer.rb
+++ config/initializers/mailboxer.rb
@@ -4,7 +4,7 @@ Mailboxer.setup do |config|
   config.uses_emails = true
 
   #Configures the default from for emails sent for Messages and Notifications
-  config.default_from = "do-not-reply@york.ac.uk"
+  config.default_from = "OASIS <oasis@oasis-database.org>"
 
   #Configures the methods needed by mailboxer
   config.email_method = :mailboxer_email
