df821ff342dd5836e9e42ab1c9e95cc6489f1271
Frank <frank.feng@york.ac.uk>
2018-07-13 13:06:33 +0000

    update mail settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,16 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # York SMTP server
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "oasis-database.org" }
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.york.ac.uk',
+      port:                 25,
+      enable_starttls_auto: true
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,11 +66,13 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # York SMTP server
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "oasis-database.org" }
   config.action_mailer.smtp_settings = {
-    address:              'smtp.york.ac.uk',
-    port:                 25,
-    enable_starttls_auto: true
+      address:              'smtp.york.ac.uk',
+      port:                 25,
+      enable_starttls_auto: true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
