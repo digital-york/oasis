36691caf735d8d7c52aaf8d651a726d27038bc70
Frank <frank.feng@york.ac.uk>
2018-07-26 13:27:17 +0000

    add 'other' fields into search parameters

--- app/search_builders/oasis/oasis_search_builder.rb
+++ app/search_builders/oasis/oasis_search_builder.rb
@@ -1,6 +1,6 @@
 module Oasis
   class OasisSearchBuilder < Hyrax::CatalogSearchBuilder
-    self.default_processor_chain += [:add_authority_label_field_to_query, :add_oasis_text_fields_to_query]
+    self.default_processor_chain += [:add_authority_label_field_to_query, :add_oasis_text_fields_to_query, :add_other_fields_to_query]
 
     # Add OASIS authority label fields to Solr query parameters
     def add_authority_label_field_to_query(solr_parameters)
@@ -15,6 +15,13 @@ module Oasis
       solr_parameters[:qf] += ' publication_title_tesim'
       solr_parameters[:qf] += ' summary_writer_tesim'
       solr_parameters[:qf] += ' title_of_summary_tesim'
+    end
+
+    # Add 'other' fields to Solr query parameters
+    def add_other_fields_to_query(solr_parameters)
+      solr_parameters[:qf] += ' summary_general_research_area_other_tesim'
+      solr_parameters[:qf] += ' publication_journal_name_other_tesim'
+      solr_parameters[:qf] += ' participants_proficiency_other_tesim'
     end
   end
 end
