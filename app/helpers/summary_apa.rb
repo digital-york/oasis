bcc0b129c63f1ff607f6f61f2d3245951654e17f
Frank <frank.feng@york.ac.uk>
2018-08-16 14:55:54 +0000

    support links for other journals

--- app/helpers/summary_apa.rb
+++ app/helpers/summary_apa.rb
@@ -20,6 +20,28 @@ class SummaryApa < Apa
         '.'
   end
 
+  def self.get_summary_with_other_journal_name_reference_html(writers,
+      summary_publication_year,
+      title,
+      oasis_publication_name,
+      authors,
+      publication_year,
+      other_journal_name,
+      other_journal_url,
+      authority_value=true)
+    j_string = html_italic(other_journal_name) + '. ';
+    j_string = html_italic(get_other_journal_string(other_journal_name, other_journal_url)) if authority_value==true
+
+    get_author_string(writers) +
+        get_summary_year_string(summary_publication_year) +
+        get_title_string(title) +
+        html_italic(oasis_publication_name) + ' of ' +
+        get_author_string_short(authors) +
+        get_publication_year_string(publication_year) + ' in ' +
+        j_string +
+        '.'
+  end
+
   def self.get_publication_year_string(year)
     if not year.nil? and not year[0].nil? and year[0]!=''
       return '(' + year[0] + ') '
