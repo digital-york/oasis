9c2a3aa8b73704246ca2d65644b8a3770e178b3a
Frank Feng <y.frank.feng@gmail.com>
2018-11-14 09:50:59 +0000

    include DownloaderMetadata

--- app/models/downloader.rb
+++ app/models/downloader.rb
@@ -10,6 +10,8 @@ class Downloader < ActiveFedora::Base
 
   self.human_readable_type = 'Downloader'
 
+  include ::Oasis::DownloaderMetadata
+
   # This must be included at the end, because it finalizes the metadata
   # schema (by adding accepts_nested_attributes)
   include ::Hyrax::BasicMetadata
