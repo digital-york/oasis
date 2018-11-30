6b714417f9b68dd6d84ec1326678ce79f2555f59
Frank Feng <y.frank.feng@gmail.com>
2018-11-30 11:41:48 +0000

    add basic controller and views

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -1,7 +1,22 @@
 class SurveyController < ApplicationController
+  protect_from_forgery
 
   with_themed_layout 'survey'
 
   def new
+  end
+
+  def submit
+    puts "==================================="
+    puts "==================================="
+
+    params.each do |key,value|
+      Rails.logger.warn "Param #{key}: #{value}"
+    end
+
+    puts "==================================="
+    puts "==================================="
+
+    redirect_to "/survey/saved?id=555"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get 'survey/new'
+  get 'survey/saved'
   post 'survey/submit'
 
   mount Blacklight::Engine => '/'
