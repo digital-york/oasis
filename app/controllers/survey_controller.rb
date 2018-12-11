054d8396ff925040c14bf8facc7dce787161bbd5
Frank Feng <y.frank.feng@gmail.com>
2018-12-11 15:38:51 +0000

    process survey form data

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -7,16 +7,17 @@ class SurveyController < ApplicationController
   end
 
   def submit
-    puts "==================================="
-    puts "==================================="
+    summary_id         = params["summary_id"]
+    summary_fileset_id = params["summary_fileset_id"]
 
-    params.each do |key,value|
-      Rails.logger.warn "Param #{key}: #{value}"
-    end
+    d = Summary.find(summary_id).downloaders.new
+    d.downloader_status      = params["survey"]["status"] unless params["survey"]["status"].nil?
+    d.downloader_email       = params["email"]            unless params["email"].nil?
+    d.downloader_affiliation = params["institution"]      unless params["institution"].nil?
+    d.downloader_contable    = params["contable"]         unless params["contable"].nil?
+    d.downloader_cty         = params["country"]          unless params["country"].nil?
+    d.save!
 
-    puts "==================================="
-    puts "==================================="
-
-    redirect_to "/survey/saved?id=555"
+    redirect_to "/survey/saved?summary_id=" + summary_id.to_s + "&downloader_id=" + d.id.to_s + "&fileset_id=" + summary_fileset_id.to_s
   end
 end
