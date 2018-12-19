b0e65783b26a7fdaa44173edd63afb55a738bfcd
Frank Feng <y.frank.feng@gmail.com>
2018-12-19 15:16:35 +0000

    remove institution,contable,country from controller

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -14,9 +14,7 @@ class SurveyController < ApplicationController
       d = Summary.find(summary_id).downloaders.new
       d.downloader_status      = params["survey"]["status"] unless params["survey"]["status"].nil?
       d.downloader_email       = params["email"]            unless params["email"].nil?
-      d.downloader_affiliation = params["institution"]      unless params["institution"].nil?
-      d.downloader_contable    = params["contable"]         unless params["contable"].nil?
-      d.downloader_cty         = params["country"]          unless params["country"].nil?
+
       d.save!
 
       redirect_to '/survey/saved?survey_done=yes&summary_id=' + summary_id.to_s + '&downloader_id=' + d.id.to_s + '&fileset_id=' + summary_fileset_id.to_s
