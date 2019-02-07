776be15fa653216bdea9afd93b11e9f507f5dd42
straccers <ps552@york.ac.uk>
2019-02-07 11:52:50 +0000

    add countries dropdown to survey
    added dropdown populated from countries authority to survey form and 
    updated survey_controler and downloader_metadata accordingly

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -10,11 +10,12 @@ class SurveyController < ApplicationController
     begin
       summary_id         = params["summary_id"]
       summary_fileset_id = params["summary_fileset_id"]
+      summary_participants_country = params["participants_country"]
 
       d = Summary.find(summary_id).downloaders.new
       d.downloader_status      = params["survey"]["status"] unless params["survey"]["status"].nil?
       d.downloader_email       = params["email"]            unless params["email"].nil?
-
+      d.participants_country = summary_participants_country
       d.save!
 
       redirect_to '/survey/saved?survey_done=yes&summary_id=' + summary_id.to_s + '&downloader_id=' + d.id.to_s + '&fileset_id=' + summary_fileset_id.to_s
