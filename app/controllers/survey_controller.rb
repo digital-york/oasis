27f00bae0febd934664d7fbb906efcb70a5227c3
Sebastian Pałucha <sebastian.palucha@york.ac.uk>
2019-02-14 10:39:00 +0000

    Correct params hash name
    - Fix email params misconfiguraiton

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -12,7 +12,7 @@ class SurveyController < ApplicationController
     d = Summary.find(summary_id).downloaders.new
     d.downloader_status      = params['survey']['status'] unless params['survey']['status'].nil?
     d.participants_country   = params['survey']['participants_country'] unless params['survey']['participants_country'].nil?
-    d.downloader_email       = params['email'] unless params['email'].nil?
+    d.downloader_email       = params['survey']['email'] unless params['survey']['email'].nil?
     d.save!
 
     redirect_to '/survey/saved?survey_done=yes&summary_id=' + summary_id.to_s + '&downloader_id=' + d.id.to_s + '&fileset_id=' + summary_fileset_id.to_s
