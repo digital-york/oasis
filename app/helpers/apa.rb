fa8415a133066aeddf3b8fd6658d35c8134858c5
Sebastian Pałucha <sebastian.palucha@york.ac.uk>
2019-03-26 15:50:02 +0000

    run Atom beautify
    Code changes as suggested by Linter.

--- app/helpers/apa.rb
+++ app/helpers/apa.rb
@@ -1,52 +1,50 @@
 class Apa
   def self.get_surname(name)
     if name.include? ','
-      return name.split(',')[0].strip
+      name.split(',')[0].strip
     else
-      return name.strip
+      name.strip
     end
   end
 
   def self.get_initial(name)
     if name.include? ','
-      return name.split(',')[1].strip[0,1].upcase + '.'
+      name.split(',')[1].strip[0, 1].upcase + '.'
     else
-      return ''
+      ''
     end
   end
 
   def self.format_name(name)
-    return get_surname(name) + ', ' + get_initial(name)
+    get_surname(name) + ', ' + get_initial(name)
   end
 
   # author_all can be passed as an Array, or a string containing all authors, e.g. ["Author1","Author2","Author3",]
   def self.get_apa_short_author_all_in_one(author_all, publication_year)
     authors = author_all
     if authors.is_a? String
-      authors = author_all.strip.split('"').delete_if { |a| a=='['or a==']' or a==',' }
+      authors = author_all.strip.split('"').delete_if { |a| (a == '[') || a == ']' || (a == ',') }
     end
-    authors_s = ""
+    authors_s = ''
 
-    return authors[0] + ' (' + publication_year + ')' if authors.length==1
+    return authors[0] + ' (' + publication_year + ')' if authors.length == 1
 
     authors.each_with_index do |author, index|
-      authors_s = authors_s + author + ', ' if index<authors.length-1
-      authors_s = authors_s + '& ' + author  if index==authors.length-1
+      authors_s = authors_s + author + ', ' if index < authors.length - 1
+      authors_s = authors_s + '& ' + author if index == authors.length - 1
     end
 
-    return authors_s  + ' (' + publication_year + ')'
+    authors_s + ' (' + publication_year + ')'
   end
 
   def self.get_apa_short_html(authors, publication_year)
-    if authors.nil? or publication_year.nil?
-      return ''
-    end
+    return '' if authors.nil? || publication_year.nil?
 
-    if authors.length==1
+    if authors.length == 1
       return get_surname(authors[0]) + ' (' + publication_year + ')'
-    elsif authors.length==2
+    elsif authors.length == 2
       return get_surname(authors[0]) + ' & ' + get_surname(authors[1]) + ' (' + publication_year + ')'
-    elsif authors.length>2
+    elsif authors.length > 2
       return get_surname(authors[0]) + ' et al. (' + publication_year + ')'
     else
       return ''
@@ -54,73 +52,74 @@ class Apa
   end
 
   def self.get_doi_string(doi)
-    return "" if doi.nil? or doi==''
-    return " doi: " + doi
+    return '' if doi.nil? || (doi == '')
+
+    ' doi: ' + doi
   end
 
-  def self.get_reference_html(authors, publication_year, title, journal, authority_value=true, volume, issue, page_from, page_to,doi)
-    j_string = html_italic(journal) + '. ';
-    j_string = html_italic(get_journal_string(journal)) if authority_value==true
+  def self.get_reference_html(authors, publication_year, title, journal, authority_value = true, volume, issue, page_from, page_to, doi)
+    j_string = html_italic(journal) + '. '
+    j_string = html_italic(get_journal_string(journal)) if authority_value == true
     get_author_string(authors) +
-        get_publication_year_string(publication_year) +
-        get_title_string(title) +
-        j_string+
-        get_vol_issue_string(volume, issue) +
-        get_pages_string(page_from,page_to) +
-        get_doi_string(doi)
+      get_publication_year_string(publication_year) +
+      get_title_string(title) +
+      j_string +
+      get_vol_issue_string(volume, issue) +
+      get_pages_string(page_from, page_to) +
+      get_doi_string(doi)
   end
 
   def self.get_other_journal_string(other_journal_name, other_journal_url)
-    if not other_journal_name.nil? and other_journal_name!=''
+    if !other_journal_name.nil? && (other_journal_name != '')
       return get_html_link(other_journal_url, other_journal_name)
     end
+
     ''
   end
 
-  def self.get_reference_with_other_journal_name_html(authors, publication_year, title, other_journal_name, other_journal_url, authority_value=true, volume, issue, page_from, page_to,doi)
-    j_string = html_italic(other_journal_name) + '. ';
-    j_string = html_italic(get_other_journal_string(other_journal_name, other_journal_url)) if authority_value==true
+  def self.get_reference_with_other_journal_name_html(authors, publication_year, title, other_journal_name, other_journal_url, authority_value = true, volume, issue, page_from, page_to, doi)
+    j_string = html_italic(other_journal_name) + '. '
+    j_string = html_italic(get_other_journal_string(other_journal_name, other_journal_url)) if authority_value == true
     get_author_string(authors) +
-        get_publication_year_string(publication_year) +
-        get_title_string(title) +
-        j_string+
-        get_vol_issue_string(volume, issue) +
-        get_pages_string(page_from,page_to) +
-        get_doi_string(doi)
+      get_publication_year_string(publication_year) +
+      get_title_string(title) +
+      j_string +
+      get_vol_issue_string(volume, issue) +
+      get_pages_string(page_from, page_to) +
+      get_doi_string(doi)
   end
 
   def self.html_italic(s)
     if s.is_a? String
-      return '<i>' + s + '</i>'
+      '<i>' + s + '</i>'
     else
-      return '<i>' + s[0] + '</i>'
+      '<i>' + s[0] + '</i>'
     end
   end
 
   def self.get_author_string(authors)
     # if gets a string, e.g. author_all, just return it directly, as a temporary solution for: https://github.com/digital-york/oasis/issues/29
     if authors.is_a? String
-      return authors + ' '
+      authors + ' '
     elsif authors.length == 1
-      return authors[0] + ' '
+      authors[0] + ' '
     elsif authors.length == 2
-      return authors[0] + ' & ' + authors[1] + ' '
+      authors[0] + ' & ' + authors[1] + ' '
     elsif authors.length == 3
-      return authors[0] + ', ' + authors[1] + ' & ' + authors[2] + ' '
+      authors[0] + ', ' + authors[1] + ' & ' + authors[2] + ' '
     else
-      return authors[0] + ' et al. '
+      authors[0] + ' et al. '
     end
   end
 
   def self.get_publication_year_string(year)
-    if not year.nil? and year!=''
-      return '(' + year + '). '
-    end
+    return '(' + year + '). ' if !year.nil? && (year != '')
+
     ''
   end
 
   def self.get_title_string(title)
-    if not title.nil?
+    unless title.nil?
       if title.end_with? '.'
         return title + ' '
       else
@@ -130,32 +129,34 @@ class Apa
     ''
   end
 
-  def self.get_html_link(url, text, target='_blank')
-    return '<a href="'+url+'" target="' + target+ '">'+text+'</a>'
+  def self.get_html_link(url, text, target = '_blank')
+    '<a href="' + url + '" target="' + target + '">' + text + '</a>'
   end
 
   def self.get_journal_string(journal)
     unless journal.nil?
-      if journal.starts_with? 'http' or journal.to_i <= 0
-        return get_html_link(journal, GenericLocalAuthorityService.id_to_label('journals',journal)) + ', '
+      if journal.starts_with?('http') || (journal.to_i <= 0)
+        return get_html_link(journal, GenericLocalAuthorityService.id_to_label('journals', journal)) + ', '
       else
-        return get_html_link(journal, GenericLocalAuthorityService.id_to_label('journals',journal.to_i)) + ', '
+        return get_html_link(journal, GenericLocalAuthorityService.id_to_label('journals', journal.to_i)) + ', '
       end
     end
     ''
   end
 
-  def self.get_vol_issue_string(vol,issue)
-    unless vol.nil? or issue.nil?
-      return html_italic(vol) + '('+issue+')'+', '
+  def self.get_vol_issue_string(vol, issue)
+    unless vol.nil? || issue.nil?
+      return html_italic(vol) + '(' + issue + ')' + ', '
     end
+
     ''
   end
 
-  def self.get_pages_string(page_from,page_to)
-    if not page_from.nil? and page_from!='' and not page_to.nil? and page_to!=''
-      return page_from + '-'+page_to+'.'
+  def self.get_pages_string(page_from, page_to)
+    if !page_from.nil? && (page_from != '') && !page_to.nil? && (page_to != '')
+      return page_from + '-' + page_to + '.'
     end
+
     ''
   end
 end
--- app/helpers/summary_apa.rb
+++ app/helpers/summary_apa.rb
@@ -1,66 +1,65 @@
 class SummaryApa < Apa
-
   def self.get_summary_reference_html(writers,
-                                       summary_publication_year,
-                                       title,
-                                       oasis_publication_name,
-                                       authors,
-                                       publication_year,
-                                       journal,
-                                       authority_value=true)
-    j_string = html_italic(journal) + '. ';
-    j_string = html_italic(get_journal_string(journal)) if authority_value==true
+                                      summary_publication_year,
+                                      title,
+                                      oasis_publication_name,
+                                      authors,
+                                      publication_year,
+                                      journal,
+                                      authority_value = true)
+    j_string = html_italic(journal) + '. '
+    j_string = html_italic(get_journal_string(journal)) if authority_value == true
 
     get_author_string(writers) +
-        get_summary_year_string(summary_publication_year) +
-        get_title_string(title) +
-        html_italic(oasis_publication_name) + ' of ' +
-        get_author_string(authors) +
-        get_publication_year_string(publication_year) + ' in ' +
-        j_string +
-        '.'
+      get_summary_year_string(summary_publication_year) +
+      get_title_string(title) +
+      html_italic(oasis_publication_name) + ' of ' +
+      get_author_string(authors) +
+      get_publication_year_string(publication_year) + ' in ' +
+      j_string +
+      '.'
   end
 
   def self.get_summary_with_other_journal_name_reference_html(writers,
-      summary_publication_year,
-      title,
-      oasis_publication_name,
-      authors,
-      publication_year,
-      other_journal_name,
-      other_journal_url,
-      authority_value=true)
-      j_string = html_italic(other_journal_name) + '. ';
-      j_string = html_italic(get_other_journal_string(other_journal_name, other_journal_url)) if authority_value==true
+                                                              summary_publication_year,
+                                                              title,
+                                                              oasis_publication_name,
+                                                              authors,
+                                                              publication_year,
+                                                              other_journal_name,
+                                                              other_journal_url,
+                                                              authority_value = true)
+    j_string = html_italic(other_journal_name) + '. '
+    j_string = html_italic(get_other_journal_string(other_journal_name, other_journal_url)) if authority_value == true
 
-      get_author_string(writers) +
-        get_summary_year_string(summary_publication_year) +
-        get_title_string(title) +
-        html_italic(oasis_publication_name) + ' of ' +
-        get_author_string(authors) +
-        get_publication_year_string(publication_year) + ' in ' +
-        j_string +
-        '.'
+    get_author_string(writers) +
+      get_summary_year_string(summary_publication_year) +
+      get_title_string(title) +
+      html_italic(oasis_publication_name) + ' of ' +
+      get_author_string(authors) +
+      get_publication_year_string(publication_year) + ' in ' +
+      j_string +
+      '.'
   end
 
   def self.get_publication_year_string(year)
     return '' if year.nil?
+
     '(' + year + ') '
   end
 
   def self.get_summary_year_string(year)
-    if not year.nil? and year!=''
-      return '(' + year + '). '
-    end
+    return '(' + year + '). ' if !year.nil? && (year != '')
+
     ''
   end
 
   def self.get_journal_string(journal)
     unless journal.nil?
-      if journal.starts_with? 'http' or journal.to_i <= 0
-        return get_html_link(journal, GenericLocalAuthorityService.id_to_label('journals',journal))
+      if journal.starts_with?('http') || (journal.to_i <= 0)
+        return get_html_link(journal, GenericLocalAuthorityService.id_to_label('journals', journal))
       else
-        return get_html_link(journal,GenericLocalAuthorityService.id_to_label('journals',journal.to_i))
+        return get_html_link(journal, GenericLocalAuthorityService.id_to_label('journals', journal.to_i))
       end
     end
     ''
