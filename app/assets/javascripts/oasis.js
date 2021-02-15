789d485b13b01009fd0b92f3731c5c77948251cc
Frank Feng <frank.feng@york.ac.uk>
2021-02-15 10:23:15 +0000

    check if tinymce is active in the current page before init

--- app/assets/javascripts/oasis.js
+++ app/assets/javascripts/oasis.js
@@ -1,12 +1,17 @@
 $(function() {
     // Force load TineMCE option, work arround Hyrax bug.
-    tinyMCE.init({
-        selector: "textarea.tinymce",
-        toolbar: "styleselect | bold italic | bullist numlist | alignleft aligncenter alignright alignjustify | outdent indent | link image codesample | code ",
-        plugins: "image,link,code,codesample,autoresize,imagetools,media,table,insertdatetime,charmap,print,preview,anchor,searchreplace,visualblocks,fullscreen,advlist,lists",
-        advlist_bullet_styles: 'square',
-        advlist_number_styles: 'lower-alpha,lower-roman,upper-alpha,upper-roman'
-    })
+    tinymceActive = (typeof tinyMCE != 'undefined') && tinyMCE.activeEditor && !tinyMCE.activeEditor.isHidden();
+
+    // Check if tinymce is active in the current page
+    if(tinymceActive) {
+        tinyMCE.init({
+            selector: "textarea.tinymce",
+            toolbar: "styleselect | bold italic | bullist numlist | alignleft aligncenter alignright alignjustify | outdent indent | link image codesample | code ",
+            plugins: "image,link,code,codesample,autoresize,imagetools,media,table,insertdatetime,charmap,print,preview,anchor,searchreplace,visualblocks,fullscreen,advlist,lists",
+            advlist_bullet_styles: 'square',
+            advlist_number_styles: 'lower-alpha,lower-roman,upper-alpha,upper-roman'
+        })
+    }
 
     // Use the accordion effect on help pages.
     $("#accordion").accordion({
