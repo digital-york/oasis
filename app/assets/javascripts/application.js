4b18b58cb571bb16fdc53ace3d736f60ae1f936b
Sebastian Pałucha <sebastian.palucha@york.ac.uk>
2021-02-19 12:05:12 +0000

    Disable use of Turbolinks
    Turbolinks does not work with our JQuery oasis.js.
    On Hyrax submit form following will not work as expected:
    - other journals fields
    - hidden *_all form data
    - reordering authors and writers fields
    
    Using jquery.turbolinks gem will not solve the issue. Using page on.load event will not solve the issue.
    - https://thoughtbot.com/upcase/videos/turbolinks
    - https://github.com/kossnocorp/jquery.turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//=
-//= require turbolinks
+//Turbolinks does not work with our JQuery oasis.js. Using jquery.turbolinks gem will not solve the issue.
+//require turbolinks
 //
 // Required by Blacklight
 //= require jquery
