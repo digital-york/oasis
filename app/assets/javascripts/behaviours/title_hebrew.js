7c244c9704cb08a90a0730006ab5926cfd13b2e3
Sebastian Pałucha <sebastian.palucha@york.ac.uk>
2022-01-17 18:40:42 +0000

    Support Hebrew direction on search page
    Modify search result display for Hebrew text.

--- app/assets/javascripts/behaviours/title_hebrew.js
+++ app/assets/javascripts/behaviours/title_hebrew.js
@@ -2,11 +2,21 @@ $(document).on("turbolinks:load ready", function () {
   // path = "/concern/summaries/9593tv41j".split('/')
   // Array(4) [ "", "concern", "summaries", "9593tv41j" ]
   var path = window.location.pathname.split("/");
+
   // on /concern/summaries/XXXX page
   if (path[2] == "summaries") {
     // Check if title contains hebrew characters
     if (/[\u0590-\u05FF]/.test($("h1").text())) {
       $("h1").css("direction", "rtl");
     }
+  }
+
+  // on /catalog page
+  if (path[1] == "catalog") {
+    $("h3").each(function (index) {
+      if (/[\u0590-\u05FF]/.test($(this).text())) {
+        $(this).css("direction", "rtl");
+      }
+    });
   }
 });
