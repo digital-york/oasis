56cd3331c4eb60c90545224da3d6607326c56b9a
Sebastian Pałucha <sebastian.palucha@york.ac.uk>
2022-02-07 15:37:38 +0000

    On video play record GA event
    Trigger new GA event on video play.

---
+++ app/assets/javascripts/behaviours/video_play.js
@@ -0,0 +1,16 @@
+// on video play trigger GA events -> files -> played  
+$(document).on("turbolinks:load ready", function () {
+  let vid = document.getElementById("video_download");
+
+  vid.onplay = function () {
+    _gaq.push(["_trackEvent", "Files", "Played", $(this).data("label"), 1]);
+
+    // send journals info to GA
+    let journals = $("#summary_journals").val();
+    if (journals) {
+      journals.split("|").forEach(function (item, index) {
+        _gaq.push(["_trackEvent", "Journals", "Metadata", item, , true]);
+      });
+    }
+  };
+});
