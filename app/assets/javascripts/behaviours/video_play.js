2f79deba67c667bc6ad22d3c197cc056030e1987
Sebastian Pałucha <sebastian.palucha@york.ac.uk>
2022-02-10 17:04:10 +0000

    Check video tag for null

--- app/assets/javascripts/behaviours/video_play.js
+++ app/assets/javascripts/behaviours/video_play.js
@@ -1,16 +1,18 @@
-// on video play trigger GA events -> files -> played  
+// on video play trigger GA events -> files -> played
 $(document).on("turbolinks:load ready", function () {
   let vid = document.getElementById("video_download");
 
-  vid.onplay = function () {
-    _gaq.push(["_trackEvent", "Files", "Played", $(this).data("label"), 1]);
+  if (vid !== null) {
+    vid.onplay = function () {
+      _gaq.push(["_trackEvent", "Files", "Played", $(this).data("label"), 1]);
 
-    // send journals info to GA
-    let journals = $("#summary_journals").val();
-    if (journals) {
-      journals.split("|").forEach(function (item, index) {
-        _gaq.push(["_trackEvent", "Journals", "Metadata", item, , true]);
-      });
-    }
-  };
+      // send journals info to GA
+      let journals = $("#summary_journals").val();
+      if (journals) {
+        journals.split("|").forEach(function (item, index) {
+          _gaq.push(["_trackEvent", "Journals", "Metadata", item, , true]);
+        });
+      }
+    };
+  }
 });
