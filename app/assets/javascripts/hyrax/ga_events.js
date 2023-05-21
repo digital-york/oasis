89c5b772e051a26c3d40a813f6e077ac7ab54a44
Sebastian Pałucha <sebastian.palucha@york.ac.uk>
2023-05-21 20:55:12 +0100

    Upgrade Google Analytics events
    Use GTag API, Replace deprecated _gaq.push with gtag() function to send Google Analytics events

--- app/assets/javascripts/hyrax/ga_events.js
+++ app/assets/javascripts/hyrax/ga_events.js
@@ -3,15 +3,33 @@
 // Note: there is absence of testing here.  I'm not sure how or to what extent we can test what's getting
 // sent to Google Analytics.
 
-$(document).on('click', '#file_download', function(e) {
-  _gaq.push(['_trackEvent', 'Files', 'Downloaded', $(this).data('label'), 1]);
+// $(document).on('click', '#file_download', function(e) {
+//   _gaq.push(['_trackEvent', 'Files', 'Downloaded', $(this).data('label'), 1]);
+
+//   // send journals info to GA
+//   let journals = $('#summary_journals').val();
+//   if(journals) {
+//     journals.split("|").forEach(function (item, index) {
+//       _gaq.push(['_trackEvent', 'Journals', 'Metadata', item, ,true]);
+//     });
+//   }
+// });
+
+$(document).on("click", "#file_download", function (e) {
+  gtag("event", "Downloaded", {
+    event_category: "Files",
+    event_label: $(this).data("label"),
+  });
 
   // send journals info to GA
-  let journals = $('#summary_journals').val();
-  if(journals) {
+  let journals = $("#summary_journals").val();
+  if (journals) {
     journals.split("|").forEach(function (item, index) {
-      _gaq.push(['_trackEvent', 'Journals', 'Metadata', item, ,true]);
+      gtag("event", "Metadata", {
+        event_category: "Journals",
+        event_label: item,
+        non_interaction: true,
+      });
     });
   }
 });
-
