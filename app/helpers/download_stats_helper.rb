735eb6ce3e08f76f0b3a16f7b31047f0ce639a8a
Sebastian Pałucha <sebastian.palucha@york.ac.uk>
2023-10-03 17:07:05 +0100

    fix: Check json errors
    Gracefully rescue erors when
    - json file is empty
    - json structure is malformed

--- app/helpers/download_stats_helper.rb
+++ app/helpers/download_stats_helper.rb
@@ -1,11 +1,18 @@
 module DownloadStatsHelper
   def self.total_downloads
+    # Number of GA downlowds before GA4
+    default_number_of_downloads = '53,962'
     if Figaro.env.download_stats_json.nil?
-      '53,962'
+      default_number_of_downloads
     else
-      json_string = File.read(Figaro.env.download_stats_json)
-      # Read number and format it with comma every thousend 
-      JSON.parse(json_string)['total_downloads'].to_s.reverse.gsub(/(\d{3})(?=\d)/, '\1,').reverse
+      begin
+        json_string = File.read(Figaro.env.download_stats_json)
+        # Read number and format it with comma every thousend
+        JSON.parse(json_string)['total_downloads'].to_s.reverse.gsub(/(\d{3})(?=\d)/, '\1,').reverse
+      rescue StandardError => e
+        Rails.logger.error "DownloadStatsHelper.total_downloads => JSON parsing error: #{e.message}"
+        default_number_of_downloads
+      end
     end
   end
 end
